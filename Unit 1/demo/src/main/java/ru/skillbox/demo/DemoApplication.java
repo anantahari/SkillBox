6699f6397fe23da13ab3693cdf723f22c3912a21
anantahari <anantahari@yandex.ru>
2023-12-03 14:19:00 +0300

    New

--- Unit 1/demo/src/main/java/ru/skillbox/demo/DemoApplication.java
+++ Unit 1/demo/src/main/java/ru/skillbox/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package ru.skillbox.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+import ru.skillbox.demo.repository.UserRepository;
+import ru.skillbox.demo.entity.User;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner demoJpa(UserRepository repository) {
+		return (args) -> {
+			User vasya = new User("Vasya", "Petrov");
+			User petya = new User("Petya", "Ivanov");
+
+			repository.save(vasya);
+			repository.save(petya);
+
+			repository.deleteById(1L);
+
+			for (User user: repository.findAll()) {
+				System.out.println(user);
+			}
+		};
 	}
 
 }
